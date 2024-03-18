6adbb2d24df5a234937fa3ff1c24f2244894dcdf
Yu-Hayate <112352934+Yu-Hayate@users.noreply.github.com>
2024-03-18 16:56:39 -0400

    Update scripts.js
    added 2x2 and 16x16 Brayer matrix Functions

--- scripts.js
+++ scripts.js
@@ -50,6 +50,10 @@ function processImage() {
                 stuckiDithering(ctx, canvas.width, canvas.height);
             } else if (ditheringOption == 'Burkes') {
                 BurkesDithering(ctx, canvas.width, canvas.height);
+            } else if (ditehringOption == 'bayerMatrix2') {
+                bayerDithering2x2(ctx, canvas.width, canvas.height)
+            } else if (ditehringOption == 'bayerMatrix16') {
+                bayerDithering16x16(ctx, canvas.width, canvas.height)
             }
             outputImage.src = canvas.toDataURL();
         };
@@ -299,6 +303,45 @@ function noneDithering(context,w,h) {
     }
     imageString += "`"; 
 }
+function bayerDithering2x2(context, w, h) {
+    const ditherMatrix = [
+        [0, 2],
+        [3, 1]
+    ];
+    
+    const palArr = removedisabledColors(hexToRgb(colors), enabledColorsList);
+    const imgData = context.getImageData(0, 0, w, h);
+    const data = imgData.data;
+    const MakecodeImg = [];
+    for (let y = 0; y < h; y++) {
+        const row = [];
+        for (let x = 0; x < w; x++) {
+            const index = (y * w + x) * 4;
+            const r = data[index];
+            const g = data[index + 1];
+            const b = data[index + 2];
+            const threshold = ditherMatrix[x % 2][y % 2] / 3;
+            let newR = r > threshold * 255 ? 255 : 0;
+            let newG = g > threshold * 255 ? 255 : 0;
+            let newB = b > threshold * 255 ? 255 : 0;
+            const newpixel = findClosest([newR, newG, newB], palArr);
+            data[index] = newpixel[0];
+            data[index + 1] = newpixel[1];
+            data[index + 2] = newpixel[2];
+            row.push(palArr.indexOf(newpixel) + 1);
+        }
+        MakecodeImg.push(row);
+    }
+    let imageString = ' = img`\n';
+    for (let rows of MakecodeImg) {
+        for (let pixel of rows) {
+            imageString += pixel.toString(16) + " ";
+        }
+        imageString += "\n";
+    }
+    imageString += "`";
+    context.putImageData(imgData, 0, 0);
+}
 function bayerDithering4x4(context, w, h) {
     const ditherMatrix = [
         [0, 8, 2, 10],
@@ -362,6 +405,59 @@ function bayerDithering8x8(context, w, h) {
             const g = data[index + 1];
             const b = data[index + 2];
             const threshold = ditherMatrix[x % 8][y % 8] / 63;
+            let newR = r > threshold * 255 ? 255 : 0;
+            let newG = g > threshold * 255 ? 255 : 0;
+            let newB = b > threshold * 255 ? 255 : 0;
+            const newpixel = findClosest([newR, newG, newB], palArr);
+            data[index] = newpixel[0];
+            data[index + 1] = newpixel[1];
+            data[index + 2] = newpixel[2];
+            row.push(palArr.indexOf(newpixel) + 1);
+        }
+        MakecodeImg.push(row);
+    }
+    let imageString = ' = img`\n';
+    for (let rows of MakecodeImg) {
+        for (let pixel of rows) {
+            imageString += pixel.toString(16) + " ";
+        }
+        imageString += "\n";
+    }
+    imageString += "`";
+    context.putImageData(imgData, 0, 0);
+}
+function bayerDithering16x16(context, w, h) {
+    const ditherMatrix = [
+        [0, 192, 48, 240, 12, 204, 60, 252, 3, 195, 51, 243, 15, 207, 63, 255],
+        [128, 64, 176, 112, 140, 76, 188, 124, 131, 67, 179, 115, 143, 79, 191, 127],
+        [32, 224, 16, 208, 44, 236, 28, 220, 35, 227, 19, 211, 47, 239, 31, 223],
+        [160, 96, 144, 80, 172, 108, 156, 92, 163, 99, 147, 83, 175, 111, 159, 95],
+        [8, 200, 56, 248, 4, 196, 52, 244, 11, 203, 59, 251, 7, 199, 55, 247],
+        [136, 72, 184, 120, 132, 68, 180, 116, 139, 75, 187, 123, 142, 78, 190, 126],
+        [40, 232, 24, 216, 36, 228, 20, 212, 43, 235, 27, 219, 39, 231, 23, 215],
+        [168, 104, 152, 88, 164, 100, 148, 84, 171, 107, 155, 91, 167, 103, 151, 87],
+        [2, 194, 50, 242, 14, 206, 62, 254, 1, 193, 49, 241, 13, 205, 61, 253],
+        [130, 66, 178, 114, 129, 65, 177, 113, 134, 70, 182, 118, 133, 69, 181, 117],
+        [34, 226, 18, 210, 46, 238, 30, 222, 33, 225, 17, 209, 45, 237, 29, 221],
+        [162, 98, 146, 82, 174, 110, 158, 94, 161, 97, 145, 81, 173, 109, 157, 93],
+        [10, 202, 58, 250, 6, 198, 54, 246, 9, 201, 57, 249, 5, 197, 53, 245],
+        [138, 74, 186, 122, 137, 73, 185, 121, 140, 76, 188, 124, 141, 77, 189, 125],
+        [42, 234, 26, 218, 38, 230, 22, 214, 41, 233, 25, 217, 37, 229, 21, 213],
+        [170, 106, 154, 90, 166, 102, 150, 86, 169, 105, 153, 89, 165, 101, 149, 85]
+    ];
+    
+    const palArr = removedisabledColors(hexToRgb(colors), enabledColorsList);
+    const imgData = context.getImageData(0, 0, w, h);
+    const data = imgData.data;
+    const MakecodeImg = [];
+    for (let y = 0; y < h; y++) {
+        const row = [];
+        for (let x = 0; x < w; x++) {
+            const index = (y * w + x) * 4;
+            const r = data[index];
+            const g = data[index + 1];
+            const b = data[index + 2];
+            const threshold = ditherMatrix[x % 16][y % 16] / 255;
             let newR = r > threshold * 255 ? 255 : 0;
             let newG = g > threshold * 255 ? 255 : 0;
             let newB = b > threshold * 255 ? 255 : 0;
